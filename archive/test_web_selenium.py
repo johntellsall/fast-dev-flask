a1e238af557cf9358ce36085c5c758b5a7e4dcf2
John Mitchell <john@johntellsall.com>
2022-05-07 08:29:54 -0700

    no selenium yet

---
+++ archive/test_web_selenium.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+@pytest.mark.skip("Selenium not supported yet")
+def test_hello():
+    driver = webdriver.Firefox()
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    driver.close()
